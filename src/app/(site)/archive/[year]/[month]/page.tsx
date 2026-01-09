6a70497db326aecc350be20bea38bd86a6a49838
Alexander Ore <aore8030@gmail.com>
2026-01-09 17:44:41 +0100

    fixed home page update issue

--- src/app/(site)/archive/[year]/[month]/page.tsx
+++ src/app/(site)/archive/[year]/[month]/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import Link from "next/link";
 import { db } from "@/lib/firebase";
 import { collection, query, where, getDocs, limit, orderBy } from "firebase/firestore";
